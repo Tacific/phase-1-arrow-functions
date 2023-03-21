a386db775867a6c6afa849e71f1d3407bf3c9b7e
Chantha Voeun <cvoeun45@gmail.com>
2023-03-20 23:54:01 -0400

    DONE

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1, num2){
+    let a = 2000;
+    let b = 100;
+    return a/b;
+}
+function square(numX){
+    let x = 2;
+    return x**2;
+}
+
+const add = (numA, numB) => numA + numB;
